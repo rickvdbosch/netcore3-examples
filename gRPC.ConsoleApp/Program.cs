e699898a681b8a9e3ff038df4b421c717d1d114f
Rick van den Bosch <rickvdbosch@outlook.com>
2019-11-11 10:24:43 +0100

    Added Console App to talk to gRPC service

---
+++ gRPC.ConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using gRPC.Service01;
+
+namespace GrpcExamples.ConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            bool quit;
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+            do
+            {
+                Console.Write("Please enter your name: ");
+                var name = Console.ReadLine();
+                quit = name.Equals("q", StringComparison.OrdinalIgnoreCase);
+                if (!quit)
+                {
+                    var reply = await client.SayHelloAsync(new HelloRequest { Name = name });
+                    Console.WriteLine("Greeting: " + reply.Message);
+                }
+            } while (!quit);
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
